9ff53254dbbd93ed429cd706e7d8a294e5df1691
Maxim Van de Wynckel <maxim.vandewynckel@gmail.com>
2017-01-16 20:43:42 +0100

    classloader change

--- src/main/java/be/maximvdw/modules/ModuleUploaderMojo.java
+++ src/main/java/be/maximvdw/modules/ModuleUploaderMojo.java
@@ -89,7 +89,7 @@ public class ModuleUploaderMojo extends AbstractMojo {
 
             URL[] urls = {new URL("jar:file:" + projectFile.getPath() + "!/")};
 
-            ClassLoader cl = URLClassLoader.newInstance(urls, ClassLoader.getSystemClassLoader());
+            ClassLoader cl = URLClassLoader.newInstance(urls, this.getClass().getClassLoader());
             while (e.hasMoreElements()) {
                 JarEntry je = e.nextElement();
                 if (je.isDirectory() || !je.getName().endsWith(".class")) {
